e7d6ac96057bb675a2fe14d4b918d1aa803577b5
Siwat Sroisuwan <siwat61719@gmail.com>
2026-02-13 12:23:08 +0700

    Modify overlap function to accept Rect pointers

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,18 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *r_1, Rect *r_2) {
+    Rect r1 = *r_1;
+    Rect r2 = *r_2;
+    
+    
+    double left   = max(r1.x, r2.x);
+    double right  = min(r1.x + r1.w, r2.x + r2.w);
+    double top    = min(r1.y, r2.y);
+    double bottom = max(r1.y - r1.h, r2.y - r2.h);
 
+    if (right <= left || top <= bottom)
+        return 0;
 
-
+    return (right - left) * (top - bottom);
 }
